493f657210a87943d36c2e538acd0961d19c5e3e
ji55 <jsimbajs@gmail.com>
2021-09-12 14:31:41 +0900

    feat: 상단 nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className="nav">
+      <Link className="navLinkLeft" to="/">
+        About Us
+      </Link>
+      <Link className="navLinkLeft" to="/">
+        Story
+      </Link>
+      <Link className="navLinkLeft" to="/">
+        Gallery
+      </Link>
+      <Link className="navLogo" to="/">
+        Liam ❤ Emma
+      </Link>
+      <Link className="navLinkRight" to="/">
+        The Wedding
+      </Link>
+      <Link className="navLinkRight" to="/">
+        Registry
+      </Link>
+      <Link className="navLinkRight" to="/">
+        RSVP
+      </Link>
+    </div>
+  )
+}
+
+export default Nav
